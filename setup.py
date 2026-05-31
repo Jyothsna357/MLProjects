0af8936e6622d50e2d75654b3fa9e72ea118500b
Jyothsna357 <jyosh357@gmail.com>
2026-05-31 18:04:16 +0530

    Data Ingestion

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import find_packages, setup
 from typing import List
 
+HYPHEN_E_DOT = '-e .'
 
-def get_requirements(file_path: str) -> list[str]:
+def get_requirements(file_path: str) -> List[str]:
     '''
     This function will return the list of requirements
     '''
@@ -11,8 +12,8 @@ def get_requirements(file_path: str) -> list[str]:
         requirements = file_obj.readlines()
         requirements = [req.replace("\n", "") for req in requirements]
 
-    if '-e .' in requirements:
-        requirements.remove('-e .')
+    if HYPHEN_E_DOT in requirements:
+        requirements.remove(HYPHEN_E_DOT)
 
     return requirements
 
